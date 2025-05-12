3eb9194f97566148d965c035f73e03168c73ebe1
Krystian Chmielewski <krychatko@gmail.com>
2025-05-13 01:21:43 +0200

    openmp sum reduction

---
+++ openmp-examples/sum_reduction.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Użycie: %s <liczba_wątków> <n>\n", argv[0]);
+        return 1;
+    }
+
+    int threads = atoi(argv[1]);
+    int n = atoi(argv[2]);
+    int *array = malloc(n * sizeof(int));
+    int sum = 0;
+
+    // Inicjalizacja tablicy
+    for (int i = 0; i < n; i++) {
+        array[i] = 1;
+    }
+
+    omp_set_num_threads(threads);
+
+    double start = omp_get_wtime();
+
+    #pragma omp parallel for reduction(+:sum)
+    for (int i = 0; i < n; i++) {
+        sum += array[i];
+    }
+
+    double end = omp_get_wtime();
+
+    printf("Suma elementów tablicy: %d\n", sum);
+    printf("Czas wykonania: %f sekund\n", end - start);
+
+    free(array);
+    return 0;
+}
